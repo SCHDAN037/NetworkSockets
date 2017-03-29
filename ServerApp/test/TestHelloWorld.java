243d30f56339f60d2b261285db616c4dc51ebdce
Daniel Schwartz <schdan037@myuct.ac.za>
2017-03-29 10:10:27 +0200

    created the seperate folder structures for different apps

---
+++ ServerApp/test/TestHelloWorld.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class TestHelloWorld {
+	
+	@Test
+	public void test() {
+		String str = "Hello World";
+		assertEquals("Hello World", str);
+	}
+
+}
