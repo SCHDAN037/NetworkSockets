672109e50d80cf3ae82a0bde38f043cc07d9584a
maxwell1992 <mntshinga1992@gmail.com>
2017-03-29 11:04:23 +0200

    add Client Main class

---
+++ ClientApp/src/ClientMain.java
@@ -0,0 +1,46 @@
+/*
+*	
+*	@author Daniel Schwartz
+*	@Version */
+
+import java.net.Socket;
+
+public class ClientMain {
+
+	ClientAPI clientApi = new ClientAPI ();
+
+	public ClientMain () {
+
+	}
+
+	public void DisplayMessage (string message, string username, Datetime date){
+		try {
+
+		}
+		catch (IOException e){
+
+		}
+	}
+
+	public boolean DisplayImageOffer (string message, string username, Datetime date) {
+		try {
+
+		}
+		catch (IOException e){
+			
+		}
+	}
+
+	public void DisplayImage (string message, string username, Datetime date){
+		try {
+
+		}
+		catch (IOException e){
+			
+		}
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
