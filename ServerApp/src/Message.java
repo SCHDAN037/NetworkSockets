13c52c15a5a3603226acd5bbc41b97ea0cf12187
Oliver <OliverF.Makins@gmail.com>
2017-03-29 11:01:00 +0200

    Basic setup of the Server side complete
    I have set up the main classes that we will need to get the server to
    run – I have not yet figured out which threads we will need and where to
    put them.

---
+++ ServerApp/src/Message.java
@@ -0,0 +1,11 @@
+
+/*
+*	This class is an abstraction of the messages that are sent between sockets
+*	@author Oliver Makins
+*	@Version 29/03/2017
+*/
+
+public class Message {
+
+
+}
--- ServerApp/src/ServerApp.java
+++ ServerApp/src/ServerApp.java
@@ -4,8 +4,9 @@
 *	@Version 19/06/2016
 */
 
-public class HelloWorld {
+public class ServerApp {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		Server server = new Server();
 	}
 }
