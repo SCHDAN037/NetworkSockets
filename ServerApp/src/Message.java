fa1e3c37c875d7b28c4203661bcf39bb60471e2d
Oliver <OliverF.Makins@gmail.com>
2017-03-29 11:31:34 +0200

    Made the message class
    I have also added the appropriate unit tests.

--- ServerApp/src/Message.java
+++ ServerApp/src/Message.java
@@ -5,7 +5,49 @@
 *	@Version 29/03/2017
 */
 
-public class Message {
 
+
+import java.util.Date; 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+class Message {
+	// values are final since they should never change
+	private final String data;
+	private final  long time;
+	private final String user;
+
+	Message( String d, long t, String u) {
+		data = d;
+		assert( data != null );
+		time = t;
+		assert( time != 0 );
+		user = u;
+		assert( user != null ); 
+	}
+
+	/* get methods
+	 */
+	public String getUser(){
+		return user;
+	}
+
+	public String getData(){
+		return data;
+	}
+
+	public long getTime() {
+		return time;
+	}
+
+	/** Method prints out <time> <user>: <data>
+	 *
+	 */
+	public String toString() {
+		Date date = new Date(time);
+		DateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+		String dateFormatted = formatter.format(date);
+		return dateFormatted + " " + user + ": " + data;
+	}
 
 }
---
+++ ServerApp/test/TestMessage.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+
+
+import java.lang.System;
+
+
+
+public class TestMessage {
+	
+	@Test
+	public void test() {
+		long t = System.currentTimeMillis();
+		Message m = new Message ("hello", t, "oliver");
+		assertTrue(m.getUser().equals("oliver"));
+		assertTrue(m.getTime() == t);
+		assertTrue(m.getData().equals("hello"));
+		System.out.println(m);
+	}
+
+}
