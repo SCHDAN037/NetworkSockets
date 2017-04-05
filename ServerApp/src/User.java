e1b8df2a60c4e1ad8291f7c38d25a1da504cf07d
maxwell1992 <mntshinga1992@gmail.com>
2017-04-05 16:53:40 +0200

    M: add code to manage input codes for image transfering/receiving

--- ServerApp/src/User.java
+++ ServerApp/src/User.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * This class contains all the info about a given user in a chat
  * it is alwasy made outside the Server, but the Server provisions the
  * User with all of it's data
@@ -17,20 +17,22 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.IOException;
-
+import java.nio.file.*;
 
 
 class User extends Thread {
 	private final BlockingQueue <Message>queue; // queue to send messages elsewhere
-	
+
 	// I will add objects for reading and writing once they are finished
 	private Socket socket; // This is a connection to the User
 	private final int id;
 	private final PrintStream ps;
 	private final BufferedReader reader;
 	private String name; // the User's name
-	
-	User(Socket s, BlockingQueue <Message> q, int i) throws IOException { 
+	private Thread fileReceiver_Thread;
+	private Thread fileTransfer_Thread;
+
+	User(Socket s, BlockingQueue <Message> q, int i) throws IOException {
 		socket = s;
 		queue = q;
 		id = i;
@@ -46,7 +48,34 @@ class User extends Thread {
 		ps.println(m.toString());
 	}
 
-	protected int getID() { return id; } 
+	protected int getID() { return id; }
+
+	public void startFileReceiverConnection (String filePath)
+	{
+		// ":send /media/image.png"
+		Path fR_filePath = Paths.get( filePath );
+
+		try {
+			fileReceiver_Thread = new FileReceiver ( fR_filePath.toString() );
+			fileReceiver_Thread.start();
+		}
+		catch (ClassNotFoundException ex) { ex.printStackTrace(); }
+		catch (IOException ex) { ex.printStackTrace(); }
+		catch (Exception ex) { ex.printStackTrace(); }
+	}
+
+	public void transferFileToClient(String hostname,int portnumber)
+	{
+		// String filename = line.split(" ")[1] ;
+		// String hostname = userSocket.getInetAddress().getHostName();
+		try {
+			fileTransfer_Thread = new FileTransfer ( hostname,portnumber);
+			fileTransfer_Thread.start();
+		}
+		catch (ClassNotFoundException ex) { ex.printStackTrace(); }
+		catch (IOException ex) { ex.printStackTrace(); }
+		catch (Exception ex) { ex.printStackTrace(); }
+	}
 
 	public void run() {
 		String line;
@@ -72,11 +101,40 @@ class User extends Thread {
 					break;
 				}
 
-				// We have now made sure it is a valid message, so we add it to the queue
-				m = new Message(line, System.currentTimeMillis(), id);
-//				System.out.println("This is what you yped: " + m);
-//				System.out.println("This is the queue" + queue);
-				queue.put(m); // add this to the queue
+				// e.g user: ":send kitty.png"
+				else if ( line.toUpperCase().contains(":SEND") ) {
+					// SAVE FILE ON file_database
+					String filename = line.substring( line.lastIndexOf(" ")+1 ) ;
+					startFileReceiverConnection("file_database/"+filename);
+					// System.out.println( line.substring(0, line.indexOf(":")) + " sending image transfer offer.." );
+					m = new Message(line, System.currentTimeMillis(), id);
+					queue.put(m); // add this to the queue
+				}
+				//e.g. user: ":Y"
+				else if ( line.toUpperCase().contains(":Y") ) {
+					String name = line.substring(0, line.indexOf(":")) ;
+					System.out.println( name + " Accepted image offer!" );
+
+					// // e.g. [00:00:00] name : :Y @hostname:127.0.0.1,port:2020
+					// m = new Message(line, System.currentTimeMillis(), id);
+					// queue.put(m); // add this to the queue
+
+					String receiver = line.substring(line.indexOf("@")+1);
+					String r_portnumber = receiver.substring( receiver.indexOf(":")+1, receiver.indexOf(",") ) ;
+					String r_hostname = receiver.substring( receiver.lastIndexOf(":")+1) ;
+
+					// TRANSFER FILE FROM SERVER TO EACH ACCEPTED CLIENT.
+
+					transferFileToClient(r_hostname, Integer.parseInt(r_portnumber));
+				}
+
+				else {
+					// We have now made sure it is a valid message, so we add it to the queue
+					m = new Message(line, System.currentTimeMillis(), id);
+	//				System.out.println("This is what you yped: " + m);
+	//				System.out.println("This is the queue" + queue);
+					queue.put(m); // add this to the queue
+			}
 
 			}
 			// close all the streams and the socket when quitting or dying
